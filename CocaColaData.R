c92df535a73f149b7412258d5fd150470cf6e255
dblander <dblander@umass.edu>
2019-04-01 20:50:29 -0400

    Inputed dAta

---
+++ CocaColaData.R
@@ -0,0 +1,2 @@
+#Task: get data into file
+disneydata <- getSymbols(Symbols = "KO", auto.assign = TRUE)
--- TeslaData.R
+++ TeslaData.R
@@ -1,1 +1,2 @@
-#test
+#Task: get data into file
+tesladata <- getSymbols(Symbols = "TSLA", auto.assign = TRUE)

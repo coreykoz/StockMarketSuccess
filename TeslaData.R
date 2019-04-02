8a4efcd4d6f67d142e0bc2793bed43086039fc26
Corey Kozlovski <corey@ckozlovski.com>
2019-04-01 21:27:44 -0400

    Tesla Update

--- TeslaData.R
+++ TeslaData.R
@@ -6,3 +6,17 @@ library("quantmod")
 #Task: get data into file
 getSymbols(Symbols = "TSLA", auto.assign = TRUE)
 plot(TSLA$TSLA.Close)
+
+TSLA.pred <- HoltWinters(TSLA$TSLA.Close, beta = FALSE, gamma = FALSE)
+TSLA.pred
+
+plot(TSLA.pred)
+
+install.packages("forecast")
+library("forecast")
+TSLA.pred2 <- forecast:::forecast.HoltWinters(TSLA.pred, h = 100)
+TSLA.pred2
+forecast:::plot.forecast(TSLA.pred2)
+
+#Task: get data into time series
+
